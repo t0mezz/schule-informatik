078ccf4dc0a50a253bf4a1f160c156df2461482c
t0mezz <acc2tom@gmail.com>
2024-05-07 23:07:48 +0200

    fancy website

--- server-communication/src/server/SessionWebServer.java
+++ server-communication/src/server/SessionWebServer.java
@@ -51,18 +51,24 @@ public class SessionWebServer extends Session {
             "<body>\n" +
             "    <div class=\"container\">\n" +
             "        <h1>Finner und seine Chatbots</h1>\n" +
+            "<img src=\"https://media.tenor.com/IgqQta2JOeMAAAAj/skull-skull-emoji.gif\">\n" +
+            "<img style=\"scale: 1.3; margin-left: 5%;\" src=\"https://i.gifer.com/XwI7.gif\">" +
             "        <div class=\"poem\">\n" +
-            "            <p>Finner, der kleine Mann, so fein,</p>\n" +
-            "            <p>Liebt es, mit Chatbots zu arbeiten.</p>\n" +
+            "            <p>Finner, der kleine Mann, so klein,</p>\n" +
+            "            <p>Liebt es, mit Chatbots zu programmieren.</p>\n" +
             "            <p>Auf ihre Hilfe kann er bauen,</p>\n" +
             "            <p>Wenn neue Programme entstehen.</p>\n" +
             "            <br>\n" +
-            "            <p>Mit Eiergerichten gut versorgt,</p>\n" +
+            "            <p>Mit Eiern in seinem Mund gut versorgt,</p>\n" +
             "            <p>Sitzt er am Rechner, stets geborgen.</p>\n" +
             "            <p>Die Bots erleichtern ihm die Müh',</p>\n" +
             "            <p>Und sein Codingtraum geht in Erfüllung.</p>\n" +
             "        </div>\n" +
             "    <img src='https://cdn.discordapp.com/attachments/864937515876876309/1154125451313299466/425ad83f4d89731c569bdbaf87a0344a.png?ex=663b1ed6&is=6639cd56&hm=649ed3851b81bdd6f23bc1ca10ecd541c4294294d435627c841c3f1caf514534&'>" +
+            "<audio controls>\n" +
+            "  <source src=\"https://t0mezz.github.io/schule-informatik/server-communication/ressources/finner-lied.mp3\" type=\"audio/mpeg\">\n" +
+            "  Your browser does not support the audio element.\n" +
+            "</audio>" +
             "    </div>\n" +
             "</body>\n" +
             "</html>";
